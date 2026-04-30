17816393ef314ddb585d69b85c0dfa8272f03402
jeakyoung <dwahn4823@gmail.com>
2026-04-30 13:35:02 +0900

    Fix Ahn / Vercel Error HotFix#4
    - Vercel Error HotFix#4

--- apps/web/src/app/BE/api-docs/route.ts
+++ apps/web/src/app/BE/api-docs/route.ts
@@ -1,13 +1,9 @@
 import { ApiReference } from "@scalar/nextjs-api-reference";
 
 export const GET = ApiReference({
-  spec: {
-    url: "/BE/docs",
-  },
+  url: "/BE/docs",
   theme: "kepler",
   darkMode: false,
   defaultOpenAllTags: true,
-  metaData: {
-    title: "Next.js Migrated API Docs",
-  },
+  pageTitle: "Next.js Migrated API Docs",
 });
